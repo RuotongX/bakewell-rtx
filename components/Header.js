1733882833ce7286e9f0b8f06ebdd75b27e826de
BakaX <37313808+RuotongX@users.noreply.github.com>
2022-09-19 19:52:44 +1200

    Finished the first page and header design
    Rebuild with library of font took some times.

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Slide from "@mui/material/Slide";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import "@fontsource/syne/700.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -38,44 +41,61 @@ HideOnScroll.propTypes = {
 };
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : 'transparent',
-    ...theme.typography.body2,
-    height: 50,
-    textAlign: 'center',
-    color: theme.palette.text.sencondary,
-  }));
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "transparent",
+  ...theme.typography.body2,
+  height: 120,
+  color: "#FFFFFF",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  letterSpacing: 3,
+}));
 
 export default function Header(props) {
+  const [display, setdisplay] = React.useState(true);
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Syne",
+      fontSize: 16,
+      color: "#FFFFFF",
+      
+    },
+  });
+
   return (
     <React.Fragment>
       <CssBaseline />
       <HideOnScroll {...props}>
-        <AppBar  color="transparent" elevation={0}>
-            
+        <AppBar color="transparent" elevation={0}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
             <Box sx={{ flexGrow: 1 }} padding={0}>
-                <Grid container spacing={0}>
-                    <Grid xs={2.25}>
-                        <Item>xs=8</Item>
-                    </Grid>
-                    <Grid xs={2.25}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid xs={3}>
-                        <Item>xs=4</Item>
-                    </Grid>
-                    <Grid xs={2.25}>
-                        <Item>xs=8</Item>
-                    </Grid>
-                    <Grid xs={2.25}>
-                        <Item>xs=8</Item>
-                    </Grid>
+              <Grid container spacing={0}>
+                <Grid xs={2.25}>
+                    <Item sx={{ boxShadow: 0 }} onClick={()=>setdisplay(!display)}>
+                      {display && <div>EXPERTISE</div>}
+                    </Item>
                 </Grid>
+                <Grid xs={2.25}>
+                  <Item sx={{ boxShadow: 0 }}>PORTFOLIO</Item>
+                </Grid>
+                <Grid xs={3}>
+                  <Item sx={{ boxShadow: 0 }}>
+                    <img src="/Logo.svg" />
+                  </Item>
+                </Grid>
+                <Grid xs={2.25}>
+                  <Item sx={{ boxShadow: 0 }}>AWARDS</Item>
+                </Grid>
+                <Grid xs={2.25}>
+                  <Item sx={{ boxShadow: 0 }}>CONTACT US</Item>
+                </Grid>
+              </Grid>
             </Box>
-            
+          </ThemeProvider>
         </AppBar>
       </HideOnScroll>
       <Toolbar />
-     
     </React.Fragment>
   );
 }
--- pages/posterSlide.jsx
+++ pages/posterSlide.jsx
@@ -1,24 +1,77 @@
-
-import Image from 'next/image'
-import bg from '../public/1.png'
-
+import Image from "next/image";
+import style from "../styles/posterSlide.module.css";
+import React from "react";
+import "@fontsource/oxygen-mono/400.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
 
 function posterslide(props) {
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Oxygen mono",
+      fontSize: 12,
+      color: "#FFFFFF",
+      
+    },
+  });
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "transparent",
+    ...theme.typography.body2,
+    height: 13,
+    color: "#FFFFFF",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    letterSpacing: 2
+    ,
+  }));
 
-    const styling = {
-        backgroundImage: `url(${bg.src})`,
-        width:"100%",
-        height:"100%"
-    }
-
-    return(
-        <Image
+  return (
+    <React.Fragment>
+      <Image
+        className={style.bgimg}
         src="/../public/1.png"
         layout="fill"
-        sizes="(max-width: 768px) 100vw,
-                (max-width: 1200px) 50vw,
-                100vw"
+        sizes="(max-width: 1080px) 100vw,
+                    (max-width: 1920px) 100vw,
+                    100vw"
       />
-    ) 
-};
+      <div className={style.container}>
+        <div className={style.details}>
+          <img src="/LogoLarge.svg" alt="next" className={style.logo} />
+          <ThemeProvider theme={theme}>
+            <Box className={style.box} sx={{ flexGrow: 2 }} padding={0}>
+              <Grid container spacing={0}>
+                <Grid xs={4}>
+                  <Item sx={{ boxShadow: 0 }} style={{justifyContent: "start"}} >+64 9 480 6800</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item sx={{ boxShadow: 0 }}>
+                  BAKEWELL-WHITE YACHT DESIGN LTD
+                  </Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item sx={{ boxShadow: 0 }} style={{justifyContent: "end"}}>EST. 1994</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item sx={{ boxShadow: 0 }} style={{justifyContent: "start"}}>INFO@BAKEWELL-WHITE.COM</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item sx={{ boxShadow: 0 }}>PACE WITH GRACE / © 2022</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item sx={{ boxShadow: 0 }} style={{justifyContent: "end"}}>AUCKLAND / NEW ZEALAND</Item>
+                </Grid>
+              </Grid>
+            </Box>
+          </ThemeProvider>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
 export default posterslide;
